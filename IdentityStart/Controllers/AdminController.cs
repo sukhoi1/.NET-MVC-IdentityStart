72426fcb4d36328d29eb1cf1023d47bdbf8ab6fe
Anton Lygin <lygin@live.com>
2016-11-15 18:45:45 +0300

    v0.003 - Create user example;

--- IdentityStart/Controllers/AdminController.cs
+++ IdentityStart/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
-﻿using System.Web;
+﻿using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 using IdentityStart.Infrastructure;
+using IdentityStart.Models;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 
 namespace IdentityStart.Controllers
@@ -15,6 +18,40 @@ namespace IdentityStart.Controllers
         public AppUserManager UserManager
         {
             get { return HttpContext.GetOwinContext().GetUserManager<AppUserManager>(); }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser() { UserName = model.Name, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error);
+            }
         }
     }
 }
---
+++ IdentityStart/Models/Createmodel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityStart.Models
+{
+    public class CreateModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
