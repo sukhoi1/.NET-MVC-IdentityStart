6b1aa237aed6ac1b04725e07d8a81fab76cd60f4
Anton Lygin <lygin@live.com>
2016-11-17 13:47:26 +0300

    v0.007 - delete user, error page example;

--- IdentityStart/Controllers/AdminController.cs
+++ IdentityStart/Controllers/AdminController.cs
@@ -46,6 +46,27 @@ namespace IdentityStart.Controllers
             return View(model);
         }
 
+        public async Task<ActionResult> Delete(string id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await UserManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("Error", result.Errors);
+                }
+            }
+            else
+            {
+                return View("Error", new string[] {"User Not Found"});
+            }
+        }
+
         private void AddErrorsFromResult(IdentityResult result)
         {
             foreach (string error in result.Errors)
